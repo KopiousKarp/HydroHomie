41bc3035bfe751ef582e32bc5f4dcc111375d263
KopiousKarp <78066794+KopiousKarp@users.noreply.github.com>
2023-05-06 14:37:18 -0400

    Switched time modules & added data log

--- hydrohomie_companion_app.py
+++ hydrohomie_companion_app.py
@@ -1,10 +1,23 @@
 import serial
-import time
+from datetime import datetime
 import math
 
 #UNIX
 btser= serial.Serial("/dev/rfcomm0", baudrate=9600)
 #Windows; may need to change comm port
 #btser= serial.Serial(port='COM9', baudrate=9600)
+time_data = []
+volume_data = []
 while(1):
-    print(time.time()," : ", btser.readline().decode())
+
+    now = datetime.now() #source 1 datetime library
+    volume_data_point = int(btser.readline().decode()) #source 2 serial port
+
+
+    #print to terminal
+    print(now.strftime('%Y-%m-%d %H:%M:%S'),":",volume_data_point)
+
+    #save the data
+    time_data.append(now)
+    volume_data.append(volume_data_point)
+
