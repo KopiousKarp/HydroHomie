9fbd4808a6f8c4a5bc9cc2d68732ebec20effc60
KopiousKarp <78066794+KopiousKarp@users.noreply.github.com>
2023-05-06 14:14:05 -0400

    Update hydrohomie_companion_app.py
    print statement was borked

--- hydrohomie_companion_app.py
+++ hydrohomie_companion_app.py
@@ -7,4 +7,4 @@ btser= serial.Serial("/dev/rfcomm0", baudrate=9600)
 #Windows; may need to change comm port
 #btser= serial.Serial(port='COM9', baudrate=9600)
 while(1):
-    print(time.time() + " : " + btser.readline().decode())
+    print(time.time()," : ", btser.readline().decode())
